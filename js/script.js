cdc9d1aed4072a5cae214733ac04b2dd81059670
Tymofii <159185555+tymofii-stepanov@users.noreply.github.com>
2024-03-21 18:38:21 +0000

    Done program calculation of area of triangle

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
+// Created by: Tymofii
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function calculates area of a triangle.
+ */
+function calculateAreaOfTriangle() {
+  // input
+  const baseOfTriangle = parseInt(document.getElementById('base-of-triangle').value)
+  const heightOfTriangle = parseInt(document.getElementById('height-of-triangle').value)
+
+  // process
+  const areaOfTriangle = (baseOfTriangle * heightOfTriangle) / 2
+
+  // output
+  document.getElementById('area').innerHTML = 'Area is: ' + areaOfTriangle + ' cm²'
+}
